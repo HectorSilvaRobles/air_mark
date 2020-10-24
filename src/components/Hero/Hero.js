1b52f5b40aecc14afe5730dfff199d8a36552a18
hsr <hectorsilvarobles@gmail.com>
2020-10-24 14:22:30 -0700

    working on hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,19 @@ import './hero.sass';
 const Hero = () => {
     return (
         <div className='hero'>
-            Hero
+            <div className='hero-shade'>
+                <div className='hero-info'>
+                    <div className='hero-text'>
+                        <h1>Enhance Your Workspace</h1>
+                        <h2>We provide the office solutions you need</h2>
+                    </div>
+                    <div className='hero-button'>
+                        <button className='hero-quote'>Get Quote</button>
+                        <button className='hero-learn'>Learn More</button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
