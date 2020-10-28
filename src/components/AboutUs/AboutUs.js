1f6ea8aacbe5d0d870654afd7192d8853bdc3099
hsr <hectorsilvarobles@gmail.com>
2020-10-28 15:49:58 -0700

    finishing qualified section

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import GetToKnow from './GetToKnow/GetToKnow'
+import Qualified from './Qualified/Qualified'
 import './aboutus.sass'
 
 const AboutUs = () => {
     return (
         <div className='aboutus'>
             <GetToKnow />
+            <Qualified />
         </div>
     )
 }
