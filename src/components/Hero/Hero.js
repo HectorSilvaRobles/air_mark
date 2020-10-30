d687dc1aa2cfa46ca48c2fb9bf90ae97bb181e50
hsr <hectorsilvarobles@gmail.com>
2020-10-29 19:25:14 -0700

    working on modals

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-scroll'
+import Modal from '@material-ui/core/Modal'
+import ModalForm from '../Modal/Modal'
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import './hero.sass';
 
 const Hero = () => {
+    // open modal
+    const [open, setOpen] = useState(false)
+    const handleOpen = () =>{
+        setOpen(true)
+    }
+    const handleClose = () =>{
+        setOpen(false)
+    }
+
     return (
         <div className='hero' id='hero'>
             <div className='hero-shade'>
@@ -11,9 +25,32 @@ const Hero = () => {
                         <h2>We provide the office solutions you need</h2>
                     </div>
                     <div className='hero-button'>
-                        <button className='hero-quote'>Get Quote</button>
-                        <button className='hero-learn'>Learn More</button>
-
+                        <button 
+                            className='hero-quote'
+                            onClick={handleOpen}
+                        >Get Quote</button>
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            closeAfterTransition
+                            className={'modal-backdrop'}
+                            BackdropComponent={Backdrop}
+                            BackdropProps={{
+                                timeout: 500,
+                            }}
+                        >   
+                            <Fade in={open}>
+                                <ModalForm onClose={handleClose} />
+                            </Fade>
+                        </Modal>
+                        <Link
+                            to='services'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                        >
+                            <button className='hero-learn'>Learn More</button>
+                        </Link>
                     </div>
                 </div>
             </div>
---
+++ src/components/Modal/View1/View1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './view1.sass'
+
+const View1 = () => {
+    return (
+        <div className='view1'> 
+
+        </div>
+    )
+}
+
+export default View1
